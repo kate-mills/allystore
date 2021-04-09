3ef182f449c33fcef07621eeb372fccca8fb29e4
Kate Mills <kate.mills1124@gmail.com>
2021-04-09 15:46:42 -0700

    Navigation Context

--- root-wrapper.js
+++ root-wrapper.js
@@ -1,5 +1,10 @@
 import React from 'react'
+import {NavigationProvider} from './src/context/navigation_context'
 
 export const wrapRootElement = ({ element }) => {
-  return <>{element}</>
+  return (
+  <NavigationProvider>
+    {element}
+  </NavigationProvider>
+  )
 }
---
+++ src/context/navigation_context.js
@@ -0,0 +1,32 @@
+import React, { useContext, useReducer, createContext } from 'react'
+import reducer from '../reducers/navigation_reducer'
+import {SIDEBAR_OPEN, SIDEBAR_CLOSE} from '../actions'
+
+
+const initialState = {
+  isSidebarOpen: false,
+}
+
+const NavigationContext = createContext()
+
+
+export const NavigationProvider = ({ children }) => {
+
+  const [state, dispatch] = useReducer(reducer, initialState)
+  const openSidebar = () => {dispatch({type:SIDEBAR_OPEN})}
+  const closeSidebar= () => {dispatch({type:SIDEBAR_CLOSE })}
+
+  return (
+    <NavigationProvider.Provider
+      value={{
+        ...state,
+        openSidebar,
+        closeSidebar,
+      }}
+    >
+      {children}
+    </NavigationProvider.Provider>
+  )
+}
+
+export const useNavigationContext=()=>{return useContext(NavigationContext)}
